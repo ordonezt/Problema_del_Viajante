c2782dc5d66db71c2ce2b7c739ea1b1e5d16e1d5
Tomás Bautista Ordóñez <tomasbautista.o@gmail.com>
2021-04-17 17:54:16 -0300

    Empiezo a escribir pseudocodigo del algoritmo, esta mal e incompleto probablemente

--- TSP.py
+++ TSP.py
@@ -35,6 +35,21 @@ print(matrix)
 #print(permutation)
 
         
-
+def tsp_resolver():
+    pass
+    # Pasos del algoritmo A*:
+    #     Armar 2 listas, una vacia y una cerrada
+    #     Asignarle todos los nodos como hijos al nodo de inicio
+    #     Ponemos el nodo de inicio en la lista abierta
+        
+    #     Mientras que haya nodos en la lista abierta:
+    #         Ordenar la lista abierta de menor (costo) a mayor
+    #         Agarrar el de menor costo y pasarlo a la lista cerrada
+            
+    #         Era el nodo destino?
+    #             No:
+    #                 Pasar todos sus hijos a la lista abierta si es que no estan
+    #             Si:
+    #                 Terminar
 
         
