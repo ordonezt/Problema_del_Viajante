8ffb68b30ccad8785b6d6c69919f1b67b76ae2ef
Tomás Bautista Ordóñez <tomasbautista.o@gmail.com>
2021-04-18 01:10:54 -0300

    Empiezo a crear el algoritmo, el primer elemento anda bien y agarra bien a los hijos

--- TSP.py
+++ TSP.py
@@ -6,50 +6,159 @@ Created on Sat Apr 17 10:44:57 2021
 """
 
 from TSP_lib import leer_lineas_archivo, calc_dim, matrix_TSP
-from python_tsp.exact import solve_tsp_dynamic_programming
+from nodos_lib import Nodo, crear_nodos
+from copy import copy
+#from python_tsp.exact import solve_tsp_dynamic_programming
 
 
-filename="TSP_IN_10.TXT"
+# filename="Recursos_de_la_catedra/Datos_no_euclidianos/TSP_IN_01.txt"
 
-lineas=leer_lineas_archivo(filename)
+# lineas=leer_lineas_archivo(filename)
 
 
-#print((lineas[0]))
-#print((lineas[1]))
-#print((lineas[2]))
+# #print((lineas[0]))
+# #print((lineas[1]))
+# #print((lineas[2]))
 
-n=calc_dim(lineas[0])
+# n=calc_dim(lineas[0])
 
-print(len(lineas))
+# # print(len(lineas))
 
-print(lineas)
+# # print(lineas)
       
-matrix=matrix_TSP(n,lineas)
+# matrix=matrix_TSP(n,lineas)
 
-print(matrix)
+# print(matrix)
 
 
-#permutation, distance = solve_tsp_dynamic_programming(matrix)
+# #permutation, distance = solve_tsp_dynamic_programming(matrix)
 
-#print("Camino")
-#print(permutation)
+# #print("Camino")
+# #print(permutation)
 
+
+# def tsp_resolver(archivo_path):
+    
+# #     # Pasos del algoritmo A*:
+# #     #     Armar 2 listas, una vacia y una cerrada
+# #     #     Asignarle todos los nodos como hijos al nodo de inicio
+# #     #     Ponemos el nodo de inicio en la lista abierta
         
-def tsp_resolver():
-    pass
-    # Pasos del algoritmo A*:
-    #     Armar 2 listas, una vacia y una cerrada
-    #     Asignarle todos los nodos como hijos al nodo de inicio
-    #     Ponemos el nodo de inicio en la lista abierta
-        
-    #     Mientras que haya nodos en la lista abierta:
-    #         Ordenar la lista abierta de menor (costo) a mayor
-    #         Agarrar el de menor costo y pasarlo a la lista cerrada
+# #     #     Mientras que haya nodos en la lista abierta:
+# #     #         Ordenar la lista abierta de menor (costo) a mayor
+# #     #         Agarrar el de menor costo y pasarlo a la lista cerrada
             
-    #         Era el nodo destino?
-    #             No:
-    #                 Pasar todos sus hijos a la lista abierta si es que no estan
-    #             Si:
-    #                 Terminar
-
+# #     #         Era el nodo destino?
+# #     #             No:
+# #     #                 Pasar todos sus hijos a la lista abierta si es que no estan
+# #     #             Si:
+# #     #                 Terminar
+    
+#     #Leemos el archivo
+#     lineas = leer_lineas_archivo(archivo_path)
+#     n = calc_dim(lineas[0])
+    
+#     #Creamos la matriz de costos
+#     matriz_costos = matrix_TSP(n, lineas)
+    
+#     #Creamos las ciudades
+#     ciudades = crear_nodos(n)
+    
+#     #Creamos las listas de ciudades abiertas y cerradas, arrancan vacias
+#     abiertas = []
+#     cerradas = []
+    
+#     #Asignar valor heuristico a cada ciudad
+#     #ToDo
+    
+#     #Colocamos la ciudad 0 en abierta y todas las demas son hijas de esta
+#     #asi arranca el algoritmo
+#     ciudad_inicio = ciudades.pop(0)
+#     # for ciudad in ciudades:
+#     #     ciudad_inicio.set_hijo(ciudad)
+    
+#     abiertas.append(ciudad_inicio)
+    
+#     while len(abiertas) != 0:
+#         #Ordenamos los nodos abiertos por funcion de costo
+#         abiertas_costos = sorted(abiertas, key=lambda x: x.f)
+#         #Por las dudas que haya costos iguales ordenamos por identificador
+#         abiertas = sorted(abiertas_costos, key=lambda x: x.identificador)
         
+#         #Sacamos la ciudad con menor costo
+#         ciudad = abiertas.pop(0)
+        
+#         if(ciudad.is_meta()):
+#             print('Terminamos')
+#             break
+#         else:
+#             #La metemos en la lista de ciudades cerradas
+#             cerradas.append(ciudad)
+            
+#             #Obtengo todo el camino actual
+#             camino = ciudad.get_herencia()
+#             camino.append(ciudad)
+            
+#             #Recorro todas las ciudades para agregar a los hijos
+#             for ciudad_no_visitada in ciudades:
+#                 #Si la ciudad no esta en la herencia es un hijo
+#                 if not any(nodo.identificador == ciudad_no_visitada.identificador for nodo in camino):
+#                     print('hijo')
+#                     ciudad.set_hijo(ciudad_no_visitada)
+            
+
+
+#Para correr el algoritmo como script descomentar
+archivo_path = 'Recursos_de_la_catedra/Datos_no_euclidianos/TSP_IN_01.txt'
+
+#Leemos el archivo
+lineas = leer_lineas_archivo(archivo_path)
+n = calc_dim(lineas[0])
+
+#Creamos la matriz de costos
+matriz_costos = matrix_TSP(n, lineas)
+
+#Creamos las ciudades
+ciudades = crear_nodos(n)
+
+#Creamos las listas de ciudades abiertas y cerradas, arrancan vacias
+abiertas = []
+cerradas = []
+
+#Asignar valor heuristico a cada ciudad
+#ToDo
+
+#Colocamos la ciudad 0 en abierta y todas las demas son hijas de esta
+#asi arranca el algoritmo
+ciudad_inicio = ciudades.pop(0)
+# for ciudad in ciudades:
+#     ciudad_inicio.set_hijo(ciudad)
+
+abiertas.append(ciudad_inicio)
+
+while len(abiertas) != 0:
+    #Ordenamos los nodos abiertos por funcion de costo
+    abiertas_costos = sorted(abiertas, key=lambda x: x.f)
+    #Por las dudas que haya costos iguales ordenamos por identificador
+    abiertas = sorted(abiertas_costos, key=lambda x: x.identificador)
+    
+    #Sacamos la ciudad con menor costo
+    ciudad = abiertas.pop(0)
+    
+    if(ciudad.is_meta()):
+        print('Terminamos')
+        break
+    else:
+        #La metemos en la lista de ciudades cerradas
+        cerradas.append(ciudad)
+        
+        #Obtengo todo el camino actual
+        camino = ciudad.get_herencia()
+        camino.append(ciudad)
+        
+        #Recorro todas las ciudades para agregar a los hijos
+        for ciudad_no_visitada in ciudades:
+            #Si la ciudad no esta en la herencia es un hijo
+            if not any(nodo.identificador == ciudad_no_visitada.identificador for nodo in camino):
+                print('hijo')
+                ciudad.set_hijo(copy(ciudad_no_visitada))
--- nodos_lib.py
+++ nodos_lib.py
@@ -8,6 +8,12 @@ Created on Sat Apr 17 15:06:38 2021
 
 class Nodo:
     hijos = []
+    padre = 0
+    f = 0
+    g = 0
+    h = 0
+    
+    tipo = ''
     
     def __init__(self, identificador):
         self.identificador = identificador
@@ -18,10 +24,24 @@ class Nodo:
     def get_padre(self):
         return self.padre
     
+    def get_herencia(self):
+        herencia = []
+        padre = self.padre
+        while padre != 0:
+            herencia.append(padre)
+            padre = padre.get_padre()
+        return herencia
+    
     def set_hijo(self, hijo):
         self.hijos.append(hijo)
         hijo.set_padre(self)
     
+    def get_costo(self):
+        self.f = self.g + self.h
+        return self.f
+    
+    def is_meta(self):
+        return self.tipo == 'Meta'
 
 def crear_nodos(cantidad):
     nodos = []
