db6861c12842efece60a1ed3201609d1260f5945
Tomás Bautista Ordóñez <tomasbautista.o@gmail.com>
2021-05-04 23:41:33 -0300

    Genero los archivos de salida, opcion de 4 heuristicas, empezando tabla de comparacion, limpieza de codigo

--- TSP.py
+++ TSP.py
@@ -5,7 +5,7 @@ Created on Sat Apr 17 10:44:57 2021
 @author: Usuario
 """
 
-from TSP_lib import leer_lineas_archivo, calc_dim, matrix_TSP
+from TSP_lib import leer_lineas_archivo, calc_dim, matrix_TSP, guardar_resultados
 from nodos_lib import Nodo, crear_nodos
 from copy import copy
 from random import random
@@ -13,8 +13,10 @@ import numpy as np
 import time
 import glob
 
-def tsp_resolver(archivo_path):
-       
+def tsp_resolver(archivo_path, tipo_heuristica=3):
+    
+    print('Procesando ' + archivo_path[-13:] + ' con heuristica {}'.format(tipo_heuristica))
+    
     #Leemos el archivo
     lineas = leer_lineas_archivo(archivo_path)
     n = calc_dim(lineas[0])
@@ -60,12 +62,14 @@ def tsp_resolver(archivo_path):
                 camino_id.append(ciudad_i.identificador)
             camino_id.reverse()
             camino_id.append(ciudad.identificador)
-            print(archivo_path)
-            print('El camino es: {}'.format(camino_id))
-            print('El costo fue: {}'.format(ciudad.g))
-            print('Nodos abiertos: {}'.format(nodos_abiertos))
-            print('Tiempo de ejecucion: {} segundos'.format(tiempo_transcurrido))
-            print('Velocidad: {} nodos/segundos'.format(nodos_abiertos/tiempo_transcurrido))
+            # print(archivo_path)
+            # print('El camino es: {}'.format(camino_id))
+            # print('El costo fue: {}'.format(ciudad.g))
+            # print('Nodos abiertos: {}'.format(nodos_abiertos))
+            # print('Tiempo de ejecucion: {} segundos'.format(tiempo_transcurrido))
+            # print('Velocidad: {} nodos/segundos'.format(nodos_abiertos/tiempo_transcurrido))
+            
+            guardar_resultados(archivo_path, ciudad, nodos_abiertos, tiempo_transcurrido, heuristica=tipo_heuristica)
             break
         else:
             #La metemos en la lista de ciudades cerradas
@@ -73,8 +77,16 @@ def tsp_resolver(archivo_path):
             
             #Obtengo todo el camino actual
             camino = ciudad.get_herencia()
+            camino_cerrado = copy(camino)
             #Agrego el nodo actual al camino
             camino.append(ciudad)
+            
+            # camino_id = []
+            # for ciudad_i in camino_cerrado:
+            #     camino_id.append(ciudad_i.identificador)
+            # camino_id.reverse()
+            # camino_id.append(ciudad.identificador)
+            # print("Agregue a cerradas lo siguiente", camino_id, "con un costo de {}".format(ciudad.get_costo()))
             
             #Si el camino ya tiene todas las ciudades agrego como hijo la meta
             n_recorridas = len(camino)
@@ -82,6 +94,14 @@ def tsp_resolver(archivo_path):
                 meta = Nodo(0, meta=True)
                 ciudad.set_hijo(meta, matriz_costos)
                 abiertas.append(meta)
+                
+                # camino_meta = meta.get_herencia()
+                # camino_id = []
+                # for ciudad_i in camino_meta:
+                #     camino_id.append(ciudad_i.identificador)
+                # camino_id.reverse()
+                # camino_id.append(meta.identificador)
+                # print("Agregue a abiertas lo siguiente", camino_id, "con un costo de {}".format(meta.get_costo()))
             
             #Recorro todas las ciudades para agregar a los hijos queno esten en el camino
             for ciudad_no_visitada in ciudades:
@@ -89,127 +109,47 @@ def tsp_resolver(archivo_path):
                 if not any(nodo.identificador == ciudad_no_visitada.identificador for nodo in camino):
                     #Hago una copia del hijo
                     hijo = copy(ciudad_no_visitada)
+                    
                     #Le cargo la heuristica
-                    hijo.h = costo_minimo * (n - n_recorridas + 1)#((n - n_recorridas + 1) ** 2) / (n+1)#(n - n_recorridas + 1) * n_recorridas / (n+1)
+                    if tipo_heuristica == 0:
+                        hijo.h = 0
+                    elif tipo_heuristica == 1:
+                        hijo.h = costo_minimo * ((n - n_recorridas + 1) ** 2) / (n+1)
+                    elif tipo_heuristica == 2:
+                        hijo.h = costo_minimo * (n - n_recorridas + 1) * n_recorridas / (n+1)
+                    elif tipo_heuristica == 3:
+                        hijo.h = costo_minimo * (n - n_recorridas + 1)
+                    else:
+                        hijo.h = 0
+                    
                     #Defino la relacion padre-hijo
                     ciudad.set_hijo(hijo, matriz_costos)
                     
                     #Inserto el nodo nuevo en la lista abierta
                     abiertas.append(hijo)
-            
+                
+                    # camino_id = []
+                    # camino_hijo = hijo.get_herencia()
+                    # for ciudad_i in camino_hijo:
+                    #     camino_id.append(ciudad_i.identificador)
+                    # camino_id.reverse()
+                    # camino_id.append(hijo.identificador)
+                    #print("Agregue a abiertas lo siguiente", camino_id, "con un costo de {}".format(hijo.get_costo()))
+    else:
+        print('Error, lista abierta vacia')
 
 def tsp_resolver_todos():
+    
     archivos = glob.glob('Recursos_de_la_catedra/Datos_no_euclidianos/*.txt')
     
     tiempo_inicial = time.perf_counter()
     
     for archivo in archivos:
-        print('#######################################')
-        print('#######################################')
-        tsp_resolver(archivo)
-    
-    print('#######################################')
-    print('#######################################\n')
+        for i in range(1,3+1): #Poner rango que 
+            if i != 2:
+                tsp_resolver(archivo, tipo_heuristica=i)
+
     tiempo_final = time.perf_counter()
-    
     tiempo_total = tiempo_final - tiempo_inicial
+    # print('Tiempo total: {}'.format(tiempo_total))
     
-    print('Tiempo total: {}'.format(tiempo_total))
-    
-# #Para correr el algoritmo como script descomentar
-# archivo_path = 'Recursos_de_la_catedra/Datos_no_euclidianos/TSP_IN_04.txt'
-
-# #Leemos el archivo
-# lineas = leer_lineas_archivo(archivo_path)
-# n = calc_dim(lineas[0])
-
-# #Creamos la matriz de costos
-# matriz_costos = matrix_TSP(n, lineas)
-
-# costo_minimo = min(matriz_costos[np.nonzero(matriz_costos)])
-
-# #Creamos las ciudades
-# ciudades = crear_nodos(n)
-
-# #Creamos las listas de ciudades abiertas y cerradas, arrancan vacias
-# abiertas = []
-# cerradas = []
-
-# #Asignar valor heuristico a cada ciudad
-# #ToDo
-
-# #Colocamos la ciudad 0 en abierta y todas las demas son hijas de esta
-# #asi arranca el algoritmo
-# ciudad_inicio = ciudades.pop(0)
-# # for ciudad in ciudades:
-# #     ciudad_inicio.set_hijo(ciudad)
-
-# abiertas.append(ciudad_inicio)
-
-# nodos_abiertos = 0
-
-# tiempo_inicial = time.perf_counter()
-
-# while len(abiertas) != 0:
-#     #Ordenamos los nodos abiertos por funcion de costo
-#     #abiertas_costos = sorted(abiertas, key=lambda x: x.f)
-#     #Por las dudas que haya costos iguales ordenamos por identificador
-#     #abiertas = sorted(abiertas_costos, key=lambda x: x.identificador)
-#     abiertas = sorted(abiertas, key=lambda x: x.f)
-    
-#     #Sacamos la ciudad con menor costo
-#     ciudad = abiertas.pop(0)
-#     nodos_abiertos = nodos_abiertos + 1
-    
-#     if(ciudad.is_meta()):
-#         tiempo_final = time.perf_counter()
-#         tiempo_transcurrido = tiempo_final - tiempo_inicial
-#         camino = ciudad.get_herencia()
-#         camino_id = []
-#         for ciudad_i in camino:
-#             camino_id.append(ciudad_i.identificador)
-#         camino_id.reverse()
-#         camino_id.append(ciudad.identificador)
-#         print(archivo_path)
-#         print('El camino es: {}'.format(camino_id))
-#         print('El costo fue: {}'.format(ciudad.g))
-#         print('Nodos abiertos: {}'.format(nodos_abiertos))
-#         print('Tiempo de ejecucion: {} segundos'.format(tiempo_transcurrido))
-#         print('Velocidad: {} nodos/segundos'.format(nodos_abiertos/tiempo_transcurrido))
-#         break
-#     else:
-#         #La metemos en la lista de ciudades cerradas
-#         cerradas.append(ciudad)
-        
-#         #Obtengo todo el camino actual
-#         camino = ciudad.get_herencia()
-#         camino.append(ciudad)
-        
-#         #Si el camino ya tiene todas las ciudades agrego como hijo la meta
-#         n_recorridas = len(camino)
-#         if n_recorridas >= n:
-#             meta = Nodo(0, meta=True)
-#             ciudad.set_hijo(meta, matriz_costos)
-#             abiertas.append(meta)
-        
-#         #Recorro todas las ciudades para agregar a los hijos queno esten en el camino
-#         for ciudad_no_visitada in ciudades:
-#             #Si la ciudad no esta en la herencia es un hijo
-#             if not any(nodo.identificador == ciudad_no_visitada.identificador for nodo in camino):
-#                 hijo = copy(ciudad_no_visitada)
-#                 hijo.h = (n - n_recorridas) * costo_minimo
-#                 ciudad.set_hijo(hijo, matriz_costos)
-                
-#                 #Recorro la lista para ver si el nodo no esta en abiertas o si esta con mayor costo
-#                 hay_que_insertar = True
-#                 # for i, ciudad_abierta in enumerate(abiertas):
-#                 #     if ciudad_abierta.identificador == hijo.identificador:
-#                 #         if ciudad_abierta.get_costo() >= hijo.get_costo():
-#                 #             hay_que_insertar = True
-#                 #             #abiertas.pop(i)                           
-#                 #         else:
-#                 #             hay_que_insertar = False
-                
-#                 if hay_que_insertar == True:
-#                     abiertas.append(hijo)
-        
--- TSP_lib.py
+++ TSP_lib.py
@@ -5,7 +5,7 @@ Created on Sat Apr 17 10:36:06 2021
 @author: Usuario
 """
 import numpy as np
-
+from datetime import timedelta
 
 def leer_archivo(filename):
         
@@ -55,3 +55,50 @@ def matrix_TSP(n,lin):
                     cont=cont+1
     
         return(matrix)
+
+def guardar_resultados(path_archivo, meta, nodos_abiertos, tiempo, heuristica=None):
+    # La información que contendrá el archivo de salida es la siguiente:
+
+    # TSP_OUT_xx_XXX.txt:
+    
+    # strCamino;<CR>
+    # CT;<CR>
+    # NA;<CR>
+    # strTiempoEj;<CR>
+    # EOF
+    
+    # Donde:   
+    # strCamino es un string con el camino encontrado, de la forma "1;2;3;4;5;1;"
+    # CT es el costo total del camino encontrado.
+    # NA es la cantidad de nodos abiertos durante el proceso.
+    # strTiempoEj es un string con el tiempo de ejecución.
+    if heuristica != None:
+        heuristica_str = str(heuristica)
+    else:
+        heuristica_str = ''
+        
+    id_archivo = path_archivo[-6:-4]
+    extension = '.txt'
+    path = 'Resultados_TSP/TSP_OUT_' + id_archivo + '_BBMO' + heuristica_str + extension
+    
+    with open(path, 'w+') as archivo:
+        
+        #formo la string del camino
+        camino = meta.get_herencia()
+        camino_id = []
+        for ciudad_i in camino:
+            camino_id.append(ciudad_i.identificador)
+        camino_id.reverse()
+        camino_id.append(meta.identificador)
+        archivo.write(str(camino_id) + ';\n')
+        
+        #Escribo el costo
+        archivo.write(str(meta.get_costo()) + ';\n')
+        
+        #Escribo la cantidad de nodos abiertos
+        archivo.write(str(nodos_abiertos) + ';\n')
+        
+        #Escribo el tiempo de ejecucion
+        tiempo_str = str(timedelta(seconds=tiempo))
+        archivo.write(tiempo_str + ';\n')
+        
